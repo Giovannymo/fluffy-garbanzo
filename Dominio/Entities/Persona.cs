7c1c8c0d7e5aa390111c6fc1d9ec65aa5108be44
Giovanny Moreno <giovannysocas@gmail.com>
2023-09-01 00:02:49 -0500

    Cambios en entities

--- Dominio/Entities/Persona.cs
+++ Dominio/Entities/Persona.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 
 namespace Dominio.Entities
 {
-    public class Person : BaseEntity
+    public class Persona : BaseEntity
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public int Dni { get; set; }
-        public ICollection<Reserva> reservas { get; set; }
+        public ICollection<Reserva> Reservas { get; set; }
         public ICollection<Alquiler> Alquileres { get; set; }
 
     }
---
+++ Dominio/Entities/SucursalAutomovil.cs
@@ -0,0 +1,10 @@
+namespace Dominio.Entities;
+
+    public class SucursalAutomovil
+    {
+        public int CantidadDisponible { get; set; }
+        public int IdSucursalFk { get; set; }
+        public Sucursal Sucursal { get; set; }
+        public ICollection<Automovil> Automoviles { get; set; }
+        
+    }
