f4d65519581a4de7949782a2ed939167f6e31924
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-31 23:43:01 -0500

    feat: Creacion Sucursal

--- Dominio/Entities/Sucursal.cs
+++ Dominio/Entities/Sucursal.cs
@@ -2,7 +2,6 @@ namespace Dominio.Entities;
 
     public class Sucursal : BaseEntity
     {
-
         public string Nombre { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
