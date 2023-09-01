0bda7e94ef7cbfaa759ed481109860cc09fe99dd
Juarika <lozadagomezj@gmail.com>
2023-08-31 21:06:12 -0500

    feat: :zap: Creacion entity RegistroEntrega
    Creacion de la entidad RegistroEntrega, las llaves foraneas y los objetos fueron creados.

--- Dominio/Entities/RegistroEntrega.cs
+++ Dominio/Entities/RegistroEntrega.cs
@@ -6,5 +6,8 @@ public class RegistroEntrega
     public DateTime FechaEntrega { get; set; }
     public decimal CombustibleEntregado { get; set; }
     public int KilometrajeEntregado { get; set; }
-    
+    public int IdAlquilerFK { get; set; }
+    public Alquiler Alquiler { get; set; }
+    public int IdPersonaFK { get; set; }
+    public Persona Persona { get; set; }
 }
