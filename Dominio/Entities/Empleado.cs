e8a1112a8b215a7cce42689b7bdf950ed34f7102
alexander-calderon <alesistemask8@gmail.com>
2023-09-01 12:34:37 -0500

    feat

--- Dominio/Entities/Empleado.cs
+++ Dominio/Entities/Empleado.cs
@@ -7,6 +7,18 @@ namespace Dominio.Entities
 {
     public class Empleado
     {
+        public int Id {get; set;}
+        public string Nombre {get; set;}
+        public string Apellido {get; set;}
+        public string Dni {get; set;}
+        public string Direccion {get; set;}
+        public string Telefono {get; set;}
+        public string Cargo {get; set;}
+
+        public override string ToString()
+        {
+            return $"Hola, Soy Mario, llámame {Telefono} (solo hombres)";
+        }
         
     }
 }
